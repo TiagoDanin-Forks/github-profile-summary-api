c9ef69aa346d267e62e8379595cbfafaa843298e
TheWillG <contact@thewillg.com>
2018-10-04 09:08:49 -0400

    remove cloudbuild and add circleci for PRs

--- __tests__/integration.spec.js
+++ __tests__/integration.spec.js
@@ -5,7 +5,7 @@ const app = require("../src/app");
 describe("Integration tests", () => {
   it("Gets 200 when querying /github/userData/${username}", done => {
     request(app)
-      .get("/api/v1/github/userData/thewillg")
+      .get("/api/v1/ping")
       .expect(200, done);
   });
 });
